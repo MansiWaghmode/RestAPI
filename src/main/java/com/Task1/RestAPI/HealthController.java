91f04db5a9481495f6f343728a6ecc0be864be7f
Mansi Waghmode <mwaghmode1805@gmail.com>
2026-01-17 22:55:34 +0530

    Plain Java REST API

---
+++ src/main/java/com/Task1/RestAPI/HealthController.java
@@ -0,0 +1,26 @@
+package com.Task1.RestAPI;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public Map<String,String> health(){
+        Map<String,String> response = new HashMap<>();
+        response.put("status","UP");
+        return response;
+    } 
+
+    @PostMapping("/echo")
+    public Map<String,Object> echo(@RequestBody Map<String,Object> request) {
+       return request;
+    }
+    
+}
---
+++ src/test/java/com/Task1/RestAPI/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Task1.RestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
